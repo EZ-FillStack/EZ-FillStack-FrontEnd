87014369d184261ee764ad5df1636acb599d6b06
bize <bizecommission@gmail.com>
2026-02-24 18:24:42 +0900

    Update: add logo svg and Logo component

---
+++ src/components/assets/Logo.tsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg';
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} width={100} height={28}/>
+        </div>
+    );
+};
+
+export default Logo;
