fc31c6c76d8b32c6e54e20abb975b0a3604fa6f2
cherry <rlathrkd2766@gmail.com>
2026-03-23 12:49:46 +0900

    refactor: 리뷰 타입 공통화 (types/review.ts)

---
+++ src/types/review.ts
@@ -0,0 +1,10 @@
+export type Review = {
+  id: number;
+  memberId: number;
+  eventId: number;
+  rating: number;
+  content: string;
+  createdAt: string;
+  nickname: string;
+  title: string;
+};
